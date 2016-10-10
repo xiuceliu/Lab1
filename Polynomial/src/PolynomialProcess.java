3a3a47ee707c518b5428aad0e496f331f59b888e
Asphelzhn <568718851@qq.com>
2016-10-10 16:46:31 +0800

    Add files via upload

---
+++ Polynomial/src/PolynomialProcess.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+public class PolynomialProcess {
+	public static void main(String[] args){
+		Polynomial polynomial = new Polynomial();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		String command = new String();
+		while (true){
+			//读取并检测, 若出错，重复读取操作
+			try {  
+			    command = br.readLine();         
+			} catch (IOException e) {  
+			    e.printStackTrace();  
+			} 
+			polynomial.expression(command);
+			
+			}
+		}
+	}
